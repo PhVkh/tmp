47d485f8d7e337c7c330b4a2f458abdbd53c76ca
flp <fvakhitov@lanit.ru>
2020-10-24 21:34:23 +0300

    выпилил eureka

--- src/main/java/fi/lipp/greatheart/directory/DirectoryApplication.java
+++ src/main/java/fi/lipp/greatheart/directory/DirectoryApplication.java
@@ -2,13 +2,9 @@ package fi.lipp.greatheart.directory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableZuulProxy
-@EnableFeignClients
-@EnableEurekaClient
 @SpringBootApplication
 public class DirectoryApplication {
 
