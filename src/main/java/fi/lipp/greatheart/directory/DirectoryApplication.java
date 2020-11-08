0f22d0b7b95834aebef8e89d7fbda7e4bdac56ce
Asgar Zagitov <ihateyouthor@gmail.com>
2020-11-08 18:08:33 +0300

    fixed name & added pid file

--- src/main/java/fi/lipp/greatheart/directory/DirectoryApplication.java
+++ src/main/java/fi/lipp/greatheart/directory/DirectoryApplication.java
@@ -2,12 +2,15 @@ package fi.lipp.greatheart.directory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class DirectoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DirectoryApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(DirectoryApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter());
+		springApplication.run(args);
 	}
 
 }
