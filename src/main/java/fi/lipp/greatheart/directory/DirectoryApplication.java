2e2de18552cf4dd4841f5adddef31330a0e47f3f
Lilia <iskhakovalilia@yandex.ru>
2020-11-16 17:32:00 +0300

    add swagger

--- src/main/java/fi/lipp/greatheart/directory/DirectoryApplication.java
+++ src/main/java/fi/lipp/greatheart/directory/DirectoryApplication.java
@@ -2,13 +2,15 @@ package fi.lipp.greatheart.directory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableZuulProxy
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableSwagger2
 public class DirectoryApplication {
 
 	public static void main(String[] args) {
